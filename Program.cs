214a6b2960d927e0607982d391f8b7e699f9e0d1
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2022-12-08 12:30:57 +0100

    felö

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+StreamReader input = new StreamReader("talfil.txt");
+List<string> Rader = new List<string>();
+string rad = "";
+while ((rad = input.ReadLine()) !=null)
+{
+    Rader.Add(rad);
+}
+input.Close();
+
+int total = Rader.Sum(rad => Convert.ToInt32(rad));
+Console.WriteLine(total);
+int total2 = Rader.Where(rad=>(Convert.ToInt32(rad)%2)==0).Sum();
+Console.WriteLine(total2);
